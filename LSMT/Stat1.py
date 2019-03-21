9a918da18e19b415d24578f190920c3220510358
Zhang <billy.zhang02@sap.com>
2019-03-21 18:09:33 +0800

    update2

--- LSMT/Stat1.py
+++ LSMT/Stat1.py
@@ -1,5 +1,6 @@
 # encoding: UTF-8
 import pandas as pd
+import numppy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
@@ -47,9 +48,24 @@ for ti in range(52500):
 
 all_mid_data = np.concatenate([train_data,test_data],axis=0)
 
-# plt.figure(figsize = (18,9))
-# plt.plot(range(df.shape[0]),(df['low']+df['high'])/2.0)
-# plt.xticks(range(0,df.shape[0],500),df['datetime'].loc[::500],rotation=45)
-# plt.xlabel('datetime',fontsize=18)
-# plt.ylabel('Mid Price',fontsize=18)
-# plt.show()
+window_size = 100
+N = train_data.size
+
+run_avg_predictions = []
+run_avg_x = []
+
+mse_errors = []
+
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
+
+decay = 0.5
+
+for pred_idx in range(1,N):
+   
+   running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
+   run_avg_predictions.append(running_mean)
+   mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
+   run_avg_x.append(date)
+
+print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(mse_errors)))
