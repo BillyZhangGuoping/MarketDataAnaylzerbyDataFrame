cc8ac0821b1785322385dfec12b0449bec076b66
Zhang <billy.zhang02@sap.com>
2019-05-15 16:19:13 +0800

    add mutated and support multiprocessing

--- PSOOptimize/PSOOptimize.py
+++ PSOOptimize/PSOOptimize.py
@@ -2,6 +2,22 @@
 
 """
 展示如何实现PSO粒子群优化VNPY策略参数
+1、静态函数object_func，本函数为优化目标函数，根据随机生成的策略参数，运行回测后自动返回1个结果指标：夏普比率 这个是直接抄GenticOptimize2V的。这个独立出来是为了之后多线程实现。这里传入的是一个[{key1:para}，{key2，para}] 这样结构的参数队列。
+
+2、类PSOOptimize，放置主要函数如下：
+2.1、__init__ 对象初始化，需要传入的参数是Strategy量化策略, Symbollist回测品种, Parameterlist参数范围。
+2.2、creator.create("FitnessMax", base.Fitness, weights=(1.0,)) 定义优化方向
+        creator.create("Particle", list, fitness=creator.FitnessMax, speed=list, pmin = list, pmax = list,smin=list, smax=list, parameterPackage = dict, best=None)
+         定义粒子类，其中包括参数list，回测返回结果，速度list，所在范围上下限队列，和速度 上下限 队列；还有一个打包的参数字典，主要为了之后多线程调用。和示例代码对比最大区别就是多了所在范围上线，因为如果没有，会出现负参数的情况报错。
+2.3、particle_generate， 生成粒子，包含随机位置，和速度；这里面只支持（start，end，pace) 这样一种参数范围赋值。start和end就是位置范围上下限。步长pace就是速度上限，负pace就是速度下限。
+2.4、updateParticle，更新粒子信息，根据粒子群最佳位置best，去更新粒子part的位置和速度。如果速度或位置在上下限，就去上下限值。如果位置值是整数，比是MAwindow这样，就会更新值也是整数。
+        速度公式：
+        v[] = v[] + c1 * rand() * (pbest[] - present[]) + c2 * rand() * (gbest[] - present[])
+        位置公式：
+        present[] = persent[] + v[]
+2.5、optimize，这个没说明好说，主要就是绑定函数到toolbox，然后定义粒子群的粒子数量，和寻觅次数。优化出最有结果。
+    使用pool.map实现了多线程
+2.6、mutated, 自变异函数
 """
 from __future__ import division
 from __future__ import print_function
@@ -13,6 +29,9 @@ from deap import creator
 from deap import tools
 from vnpy.trader.app.ctaStrategy.ctaBacktesting import BacktestingEngine, MINUTE_DB_NAME, OptimizationSetting
 from vnpy.trader.app.ctaStrategy.strategy.strategyBollChannel import BollChannelStrategy
+import multiprocessing
+import pandas as pd
+import datetime
 
 def object_func(strategy_avgTuple):
     """
@@ -75,6 +94,7 @@ class PSOOptimize(object):
     parameterlist = {}
     parameterPackage = {}
 
+
     # ------------------------------------------------------------------------
     def __init__(self, Strategy, Symbollist, Parameterlist):
         self.strategy = Strategy
@@ -92,6 +112,8 @@ class PSOOptimize(object):
     def particle_generate(self):
         """
         生产particle粒子，根据传入设置的起始值，终止值随机生成位置,和位置最大最小值，根据步进生成速度,和速度最大最小值
+        支持两种参数设置，一个是三个元素start，end 和pace
+        还有一个单一不变点
         """
         position_list = []
         speed_list = []
@@ -102,7 +124,10 @@ class PSOOptimize(object):
         for key, value in self.parameterlist.items():
             if isinstance(value, tuple):
                 if len(value) == 3:
-                    position_list.append({key:random.randrange(value[0], value[1])})
+                    if isinstance(value[0],int):
+                        position_list.append({key:random.randrange(value[0], value[1])})
+                    else:
+                        position_list.append({key: random.uniform(value[0], value[1])})
                     pmin_list.append(value[0])
                     pmax_list.append(value[1])
                     speed_list.append(random.uniform(-value[2], value[2]))
@@ -110,9 +135,15 @@ class PSOOptimize(object):
                     smax_list.append(value[2])
                 else:
                     print("Paramerte list incorrect")
+            elif isinstance(value, int) or isinstance(value, float):
+                position_list.append({key: value})
+                pmin_list.append(value)
+                pmax_list.append(value)
+                speed_list.append(0)
+                smin_list.append(0)
+                smax_list.append(0)
             else:
                 print("Paramerte list incorrect")
-
         particle = creator.Particle(position_list)
         particle.speed = speed_list
         particle.pmin = pmin_list
@@ -158,30 +189,62 @@ class PSOOptimize(object):
     def sub(self,a,b):
        return a.values()[0] - b.values()[0]
 
+    def mutated(self,particle,MUTPB):
+        """
+        自适应变异
+        :param particle: 传入
+        :param MUTPB:
+        :return:
+        """
+        size = len(particle)
+        newPart = self.particle_generate()
+        for i in xrange(size):
+            if random.random() < MUTPB:
+                particle[i] = newPart[i]
+        return particle
+
     def optimize(self):
         toolbox = base.Toolbox()
+        pool = multiprocessing.Pool(processes=(multiprocessing.cpu_count()))
+        toolbox.register("map", pool.map)
         toolbox.register("particle", self.particle_generate)
         toolbox.register("population", tools.initRepeat, list, toolbox.particle)
         toolbox.register("update", self.updateParticle, phi1=2.0, phi2=2.0)
         toolbox.register("evaluate", object_func)
 
         pop = toolbox.population(n=20) #粒子群有5个粒子
-        GEN = 10 #更新一千次
+        GEN = 20 #更新一千次
+        MUTPB = 0.1 #自适应变异概率
         best = None
         for g in range(GEN):
-            for part in pop: #每次更新，计算粒子群中最优参数，并把最优值写入best
-                part.fitness.values = toolbox.evaluate(part)
+            # for part in pop: #每次更新，计算粒子群中最优参数，并把最优值写入best
+            #     part.fitness.values = toolbox.evaluate(part)
+            #     if not part.best or part.best.fitness < part.fitness:
+            #         part.best = creator.Particle(part)
+            #         part.best.fitness.values = part.fitness.values
+            #     if not best or best.fitness < part.fitness:
+            #         best = creator.Particle(part)
+            #         best.fitness.values = part.fitness.values
+            fitnesses = toolbox.map(toolbox.evaluate, pop) #利用pool.map，实现多线程
+            for part, fit in zip(pop, fitnesses):
+                part.fitness.values = fit
                 if not part.best or part.best.fitness < part.fitness:
                     part.best = creator.Particle(part)
                     part.best.fitness.values = part.fitness.values
                 if not best or best.fitness < part.fitness:
                     best = creator.Particle(part)
                     best.fitness.values = part.fitness.values
-            for part in pop: #更新粒子位置
-                toolbox.update(part, best)
+
+            if g < GEN - 1: #在最后一轮之前，每轮进行位置更新和变异
+                for i,part in enumerate(pop):
+                    toolbox.update(part, best)#更新粒子位置
+                    if random.random() < MUTPB: #自适应变异
+                        self.mutated(part,MUTPB)
 
             # Gather all the fitnesses in one list and print the stats
         return pop, best
+
+
 
 
 if __name__ == "__main__":
@@ -191,19 +254,20 @@ if __name__ == "__main__":
     Symbol = {
             "vtSymbol": 'rb1905',
             "StartDate": "20181001",
-            "EndDate": "20190101",
-            "Slippage": 2,
+            "EndDate": "20190301",
+            "Slippage": 1,
             "Size": 10,
             "Rate": 2 / 10000,
             "Capital": 10000
         }
 
     Parameterlist = {
-        'bollWindow':(5,80,2),
-        'bollDev':(2.0,8.0,1.0),
-        'cciWindow':(5,80,2),
-        'atrWindow': (5,80,2),
-        'slMultiplier':(2.0,8.0,1.0),
+        'bollWindow':(10,50,5),
+        'bollDev':(3.0,6.0,0.6),
+        'cciWindow':(10,50,5),
+        'atrWindow':(10,50,5),
+        'slMultiplier':(3.5,5.5,0.5),
+        'fixedSize':1
     }
 
     parameterPackage = {
@@ -215,5 +279,4 @@ if __name__ == "__main__":
     pop,best = PSO.optimize()
     print ("best para: %s, result:%s" %(best,best.fitness.values))
     print(pop[:20])
-
     print("-- End of (successful) %s evolution --", Symbol["vtSymbol"])
